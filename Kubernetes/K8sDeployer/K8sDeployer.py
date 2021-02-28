99864f98613e1e2e4f259f312bc2f9d435a49cc3
Luca <luca13190@gmail.com>
2021-02-28 11:49:54 +0100

    Add pv,pvc and gateway deploy/undeploy functions

--- Kubernetes/K8sDeployer/K8sDeployer.py
+++ Kubernetes/K8sDeployer/K8sDeployer.py
@@ -19,6 +19,9 @@ def deploy_items(folder):
             if '.yaml' in file:
                 items.append(os.path.join(r, file))
 
+    if os.path.isfile(folder):
+        items.append(folder)
+
     for yaml_to_create in items:
         with open(yaml_to_create) as f:
             print(yaml_to_create)
@@ -54,6 +57,9 @@ def undeploy_items(folder):
             if '.yaml' in file:
                 items.append(os.path.join(r, file))
 
+    if os.path.isfile(folder):
+        items.append(folder)
+
     for yaml_to_create in items:
         with open(yaml_to_create) as f:
             complete_yaml = yaml.load_all(f)
@@ -75,5 +81,74 @@ def undeploy_items(folder):
                     print("######################")
 
 
+def deploy_volume(yamls):
+    print("######################")
+    print(f"We are going to DEPLOY the PersistentVolume Persi and PersistentVolumeClaim from the yaml file: {yamls}")
+    print("######################")
+
+    config.load_kube_config()
+    k8s_core_api = client.CoreV1Api()
+
+    with open(yamls) as f:
+        complete_yaml = yaml.load_all(f)
+        for partial_yaml in complete_yaml:
+            try:
+                if partial_yaml["kind"] == "PersistentVolume":
+                    api_response = k8s_core_api.create_persistent_volume(body=partial_yaml)
+                    print(f"PersistentVolume '{partial_yaml['metadata']['name']}' created.")
+                    print("---")
+                elif partial_yaml["kind"] == "PersistentVolumeClaim":
+                    api_response = k8s_core_api.create_namespaced_persistent_volume_claim(namespace=partial_yaml["metadata"]["namespace"], body=partial_yaml)
+                    print(f"PersistentVolumeClaim '{partial_yaml['metadata']['name']}' created.")
+                    print("---")
+            except ApiException as err:
+                api_exception_body = json.loads(err.body)
+                print("######################")
+                print(f"Exception raised deploying a {partial_yaml['kind']}: {api_exception_body['details']} -> {api_exception_body['reason']}")
+                print("######################")
+
+def undeploy_volume(yamls):
+    print("######################")
+    print(f"We are going to UNDEPLOY the PersistentVolume Persi and PersistentVolumeClaim from the yaml file: {yamls}")
+    print("######################")
+
+    config.load_kube_config()
+    k8s_core_api = client.CoreV1Api()
+
+    with open(yamls) as f:
+        complete_yaml = yaml.load_all(f)
+        for partial_yaml in complete_yaml:
+            try:
+                if partial_yaml["kind"] == "PersistentVolume":
+                    pv_name = partial_yaml["metadata"]["name"]
+                    resp = k8s_core_api.delete_persistent_volume(name=pv_name)
+                    print(f"Deployment '{pv_name}' deleted. Status={resp.status}")
+                elif partial_yaml["kind"] == "PersistentVolumeClaim":
+                    pvc_name = partial_yaml["metadata"]["name"]
+                    resp = k8s_core_api.delete_namespaced_persistent_volume_claim(name=pvc_name, namespace=partial_yaml["metadata"]["namespace"])
+                    print(f"Service '{pvc_name}' deleted. Status={resp.status}")
+                    print("---")
+            except ApiException as err:
+                api_exception_body = json.loads(err.body)
+                print("######################")
+                print(
+                    f"Exception raised trying to delete {partial_yaml['kind']} '{api_exception_body['details']['name']}': {api_exception_body['reason']}")
+                print("######################")
+
+
+def deploy_nginx_gateway(nginx_yaml):
+    deploy_items(nginx_yaml)
+
+def undeploy_nginx_gateway(nginx_yaml):
+    undeploy_items(nginx_yaml)
+
+
 # deploy_items("../K8sYamlBuilder/yamls")
-undeploy_items("../K8sYamlBuilder/yamls")
+# undeploy_items("../K8sYamlBuilder/yamls")
+
+# deploy_volume("../../yaml/PersistentVolumeMicroService.yaml")
+# undeploy_volume("../../yaml/PersistentVolumeMicroService.yaml")
+
+
+# deploy_nginx_gateway("../../yaml/DeploymentNginxGw.yaml")
+# undeploy_nginx_gateway("../../yaml/DeploymentNginxGw.yaml")
