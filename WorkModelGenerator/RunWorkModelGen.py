ac14b2079a14c7a7aefca08dfaae1e14df2906d3
Luca <luca13190@gmail.com>
2021-06-03 13:37:11 +0200

    Add output_path var

--- WorkModelGenerator/RunWorkModelGen.py
+++ WorkModelGenerator/RunWorkModelGen.py
@@ -1,6 +1,13 @@
 from WorkModelGenerator import get_work_model, WORKMODEL_PATH
 import json
 from pprint import pprint
+import sys
+import os
+
+if len(sys.argv) > 1:
+    parameters_file_path = sys.argv[1]
+else:
+    parameters_file_path = f'{WORKMODEL_PATH}/ServiceMeshParameters.json'
 
 try:
     with open(f'{WORKMODEL_PATH}/WorkModelParameters.json') as f:
@@ -9,6 +16,14 @@ try:
     servicemesh_file_path = params['ServiceMeshFilePath']
     with open(servicemesh_file_path) as f:
         servicemesh = json.load(f)
+    if "OutputPath" in params.keys() and len(params["OutputPath"]) > 0:
+        output_path = params["OutputPath"]
+        if output_path.endswith("/"):
+            output_path = output_path[:-1]
+        if not os.path.exists(output_path):
+            os.makedirs(output_path)
+    else:
+        output_path = WORKMODEL_PATH
 except Exception as err:
     print("ERROR: in creation of service mesh,", err)
     exit(1)
@@ -20,8 +35,8 @@ pprint(workmodel)
 keyboard_input = "y"
 
 if keyboard_input == "y":
-    with open(f"{WORKMODEL_PATH}/workmodel.json", "w") as f:
+    with open(f"{output_path}/workmodel.json", "w") as f:
         f.write(json.dumps(workmodel))
 
-    print(f"'{WORKMODEL_PATH}/workmodel.json'")
+    print(f"'{output_path}/workmodel.json'")
     print("File Saved!")
