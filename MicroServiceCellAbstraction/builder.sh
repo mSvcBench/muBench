dc558388fd31a9b84d66e49d00f9bb7c0480e36f
Luca <luca13190@gmail.com>
2021-07-23 14:56:35 +0200

    Fix Comma

--- MicroServiceCellAbstraction/builder.sh
+++ MicroServiceCellAbstraction/builder.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-docker build -t lucapetrucci/microservice_v2:latest .
-docker push lucapetrucci/microservice_v2:latest
+docker build -t msvcbench/microservice_v2:latest .
+docker push msvcbench/microservice_v2:latest
 
 echo "----------------------------------------------------------------"
 echo "----------------------------------------------------------------"
 echo "----------------------------------------------------------------"
 
-docker build -f Dockerfile.debug -t lucapetrucci/microservice_v2-ssh:latest .
-docker push lucapetrucci/microservice_v2-ssh:latest
+docker build -f Dockerfile.debug -t msvcbench/microservice_v2-ssh:latest .
+docker push msvcbench/microservice_v2-ssh:latest
