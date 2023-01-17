ae2baf91129ae97c64c38dec634817035b43a608
Andrea Detti <andrea.detti@uniroma2.it>
2023-01-17 10:04:57 +0100

    added istio auto-injection in default namespace

--- Monitoring/kubernetes-full-monitoring/monitoring-install.sh
+++ Monitoring/kubernetes-full-monitoring/monitoring-install.sh
@@ -19,6 +19,7 @@ helm install istio-base istio/base -n istio-system
 helm install istiod istio/istiod -n istio-system --wait
 kubectl create namespace istio-ingress
 kubectl label namespace istio-ingress istio-injection=enabled
+kubectl label namespace default istio-injection=enabled
 helm install istio-ingress istio/gateway -n istio-ingress
 
 # Istio - Prometeus integration
