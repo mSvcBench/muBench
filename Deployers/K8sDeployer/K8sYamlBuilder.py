c37d39455ff6a2c41c4aa21c9e9b771825c0a69f
Angelo Marchese <marchese.angelo1@gmail.com>
2024-04-22 19:37:41 +0200

    Add parameter for schedulerName property on deployment and nginx gw templates

--- Deployers/K8sDeployer/K8sYamlBuilder.py
+++ Deployers/K8sDeployer/K8sYamlBuilder.py
@@ -17,6 +17,7 @@ def customization_work_model(model, k8s_parameters):
         model[service].update({"path": k8s_parameters['path']})
         model[service].update({"image": k8s_parameters['image']})
         model[service].update({"namespace": k8s_parameters['namespace']})
+        model[service].update({"scheduler-name": k8s_parameters['scheduler-name']})
         if "replicas" in k8s_parameters.keys():
             # override replica value of workmodel.json
             model[service].update({"replicas": k8s_parameters['replicas']})
@@ -37,6 +38,7 @@ def customization_work_model(model, k8s_parameters):
 
 def create_deployment_yaml_files(model, k8s_parameters, nfs, output_path):
     namespace = k8s_parameters['namespace']
+    scheduler_name = k8s_parameters['scheduler-name']
     counter=0
     for service in model:
         counter=counter+1
@@ -45,6 +47,7 @@ def create_deployment_yaml_files(model, k8s_parameters, nfs, output_path):
             f = f.replace("{{SERVICE_NAME}}", service)
             f = f.replace("{{IMAGE}}", model[service]["image"])
             f = f.replace("{{NAMESPACE}}", namespace)
+            f = f.replace("{{SCHEDULER_NAME}}", scheduler_name)
             if "sidecar" in model[service].keys():
                 f = f.replace("{{SIDECAR}}", SIDECAR_TEMPLATE % (service, model[service]["sidecar"]))
             else:
@@ -116,6 +119,7 @@ def create_deployment_yaml_files(model, k8s_parameters, nfs, output_path):
     with open(f"{K8s_YAML_BUILDER_PATH}/Templates/DeploymentNginxGwTemplate.yaml", "r") as file:
         f = file.read()
         f = f.replace("{{NAMESPACE}}", namespace)
+        f = f.replace("{{SCHEDULER_NAME}}", scheduler_name)
 
     with open(f"{output_path}/yamls/DeploymentNginxGw.yaml", "w") as file:
         file.write(f)
